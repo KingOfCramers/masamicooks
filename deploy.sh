90a1a5fd4b7ca1ba40d168598487e3ffec9b7c22
Masami Cookson <23masam@gmail.com>
2020-07-19 17:19:18 -0400

    added comments to deploy script, made delete old files

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# This script will automatically deploy your changes to github and our production server.
+# This script will automatically deploy your changes to github.
 git add .
 git commit -m "Used deploy script: $(date)"
 git push
+
+# This will automatically build the site.
 hugo -D
 
 # Set the correct permissions and user/group for all the file upon an upload.
-rsync -rv -og --usermap="\*:www-data" --groupmap="\*:www-data" --delete --chmod=u+rwx,g+rwx,o=rx public/ slave:/var/www/masamicooks.com
+# Upload the public folder to our site and delete any (locally) deleted posts.
+rsync -rv -og --usermap="\*:www-data" --groupmap="\*:www-data" --chmod=u+rwx,g+rwx,o=rx public/ slave:/var/www/masamicooks.com --delete
